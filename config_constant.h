8ae1ce59c6b8f2518b65fc9eb989324266fe7a6f
Mustafa YILMAZ <mustafaylmz1995@gmail.com>
2026-07-20 10:53:26 +0000

    feat: add config_constant.h

---
+++ config_constant.h
@@ -0,0 +1,27 @@
+/****************************************************
+ *  config_constant.h                                         
+ *  Created on: 27-Þub-2022 17:14:06                      
+ *  Implementation of the Class config_constant       
+ *  Original author: musta                     
+ ****************************************************/
+
+#if !defined(__CONFIG_CONSTANT_H__)
+#define __CONFIG_CONSTANT_H__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h> //uint type
+
+#define MAX_SAME_PRIO_THREAD_U8 32U
+#define MAX_PRIO_U8 32U
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!defined(__CONFIG_CONSTANT_H__)*/
