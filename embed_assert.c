6824a34e5d1793a30ec18cdcea48094eb464fd3f
Mustafa YILMAZ <mustafaylmz1995@gmail.com>
2026-07-20 10:53:05 +0000

    feat: add embed_assert.c

---
+++ embed_assert.c
@@ -0,0 +1,8 @@
+/****************************************************
+ *  embed_assert.c                                         
+ *  Created on: 27-Þub-2022 17:14:06                      
+ *  Implementation of the Class embed_assert       
+ *  Original author: mustafa                     
+ ****************************************************/
+
+#include "embed_assert.h"
