6e1d2fa3c4889a2c2cbfd80b01a6c4c74d5a1e00
Mustafa YILMAZ <mustafaylmz1995@gmail.com>
2026-07-20 10:53:25 +0000

    feat: add config_constant.c

---
+++ config_constant.c
@@ -0,0 +1,8 @@
+/****************************************************
+ *  config_constant.c                                         
+ *  Created on: 27-Þub-2022 17:14:06                      
+ *  Implementation of the Class config_constant       
+ *  Original author: musta                     
+ ****************************************************/
+
+#include "config_constant.h"
